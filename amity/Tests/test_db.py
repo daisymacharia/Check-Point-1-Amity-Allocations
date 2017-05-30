384f479d28076417ffef5077cf90d045a47284ba
daisymacharia <daisy.macharia@andela.com>
2017-05-31 01:22:43 +0300

    Refactor db tests

--- amity/Tests/test_db.py
+++ amity/Tests/test_db.py
@@ -1,7 +1,9 @@
-from Controller.amity import Amity
+from amity.Controller.amity import Amity
 import unittest
+from io import StringIO
 import sys
 from os import path
+sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
 
 
 class TestDb(unittest.TestCase):
@@ -10,11 +12,13 @@ class TestDb(unittest.TestCase):
 
 
     def test_data_successfully_exported_to_database(self):
-        """ Test that data cn be loaded from the system into a database"""
-            self.assertEqual('Data successfully exported to Database',
-                             self.amity.save_state('data'))
+        """ Test that data can be loaded from the system into a database"""
+        self.amity.save_state('data')
+        message = sys.stdout.getvalue().strip()
+        self.assertIn('Data successfully exported to Database', message)
 
     def test_data_loaded_from_database(self):
         """Test that data can be loaded into the system from a database"""
-        self.assertEqual('Successfully loaded data from the Database!',
-                         self.amity.load_state('data'))
+        self.amity.load_state('data')
+        message = sys.stdout.getvalue().strip()
+        self.assertIn('Successfully loaded data from the Database.', message)
